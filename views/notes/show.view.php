e9808a03ec199521e1b08c8bcc90390242db2cce
NlcmarquesDev <nunolcmarques@gmail.com>
2024-06-25 15:43:00 +0200

    done the priority feature, need to do the delete desgin is redone

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,14 +7,14 @@ Router::showPartials('navbar');
 
 ?>
 <div class="my-5">
-    <?php if (isset($_SESSION['msg'])) : ?>
+    <?php if (isset($_SESSION['alert'])) : ?>
         <div class="alert alert-success">
-            <h4><?= $_SESSION['msg'] ?></h4>
+            <h4><?= $_SESSION['alert'] ?></h4>
         </div>
     <?php endif ?>
-    <div class="d-flex flex-wrap px-0 gap-2 mt-3">
+    <div class="d-flex justify-content-center px-0 gap-2 mt-3 row  ">
         <!-- Button trigger modal -->
-        <button type="button" class="card text-center" style="max-width: 19rem; min-width: 12rem;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="card col col-sm-6 col-md-4 col-lg-3 text-center" style="width: 19rem; height: 15rem;" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle text-primary m-auto" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
@@ -52,12 +52,23 @@ Router::showPartials('navbar');
         <?php if (!empty($notes)) : ?>
             <!-- Para fazer os alertas das mensagena ao usuário -->
             <?php foreach ($notes as $note) : ?>
-
-                <div class="card" style="max-width: 19rem;">
+                <div class="card col col-sm-6 col-md-4 col-lg-3" style="width: 19rem; height: 15rem;">
                     <div class="card-body d-flex flex-column justify-content-between">
-                        <h5 class="card-title"><?= $note['title'] ?></h5>
+                        <div class="d-flex justify-content-between ">
+                            <h5 class="card-title"><?= $note['title'] ?></h5>
+                            <span class="badge text-bg-<?= $note['priority'] === 1 ? 'danger' : 'success' ?> my-auto">Priority</span>
+                        </div>
                         <p class="card-text"><?= $note['body'] ?></p>
-                        <p class="card-text"><small class="text-body-secondary"><?= $note['date_note'] ?></small></p>
+                        <div class="d-flex justify-content-between ">
+                            <p class="card-text my-auto"><small class="text-body-secondary"><?= $note['date_note'] ?></small></p>
+                            <form action="/notes" method="POST">
+                                <input type="hidden" name="_method" value="delete">
+                                <button class="btn btn-outline-danger border-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash2-fill" viewBox="0 0 16 16">
+                                        <path d="M2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671zm9.89-.69C10.966 2.214 9.578 2 8 2c-1.58 0-2.968.215-3.926.534-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466-.18-.14-.498-.307-.975-.466z" />
+                                    </svg>
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             <?php endforeach ?>
@@ -72,11 +83,7 @@ Router::showPartials('navbar');
     </div>
 </div>
 
-
-
-
-
 <?php
-unset($_SESSION['msg']);
+unset($_SESSION['alert']);
 Router::showPartials('footer');
 ?>
